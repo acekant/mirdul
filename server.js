888d4f0ba572e3533da862c10e28c6c8e467a4fe
Dheerendra Kumar <dkuarya@gmail.com>
2024-01-28 18:29:47 +0530

    intial commit

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const fs = require('fs');
+
+const app = express();
+const port = 80;
+
+app.get('/search', (req, res) => {
+  const searchTerm = req.query;
+  const data = JSON.parse(fs.readFileSync('data.json', 'utf-8'));
+
+  // Filter data based on search parameters
+  const filteredData = data.filter(item => {
+    for (const key in searchTerm) {
+      if (item[key] !== searchTerm[key]) {
+        return false;
+      }
+    }
+    return true;
+  });
+
+  res.json(filteredData);
+});
+
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
