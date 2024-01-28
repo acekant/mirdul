9a1598f06a96a87c75bd7f13203c2863929814b5
Dheerendra Kumar <dkuarya@gmail.com>
2024-01-28 18:46:37 +0530

    added package.json

--- server.js
+++ server.js
@@ -21,6 +21,10 @@ app.get('/search', (req, res) => {
   res.json(filteredData);
 });
 
+app.get('/', (req, res) => {
+  res.send("hello world");
+})
+
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
 });
