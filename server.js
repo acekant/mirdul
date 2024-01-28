fcdd04b815fb5b72445abd868b078f7094eba3e7
Dheerendra Kumar <dkuarya@gmail.com>
2024-01-28 18:34:51 +0530

    second commit

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const fs = require('fs');
 
 const app = express();
-const port = 80;
+const port = 3000;
 
 app.get('/search', (req, res) => {
   const searchTerm = req.query;
